e49d90ffd32723afee59fcc4c4ee7250a64eca03
Mateo_Rico <matiasher0824@gmail.com>
2024-04-29 17:26:40 -0500

    Actualizacion

--- ProyectoFinalJunical/src/Entorno/entornos.ts
+++ ProyectoFinalJunical/src/Entorno/entornos.ts
@@ -2,6 +2,6 @@ export const entornos =
   {
     production: false,
 
-    dynamicHost: 'localhost:8080', // Desarrollo
-    //dynamicHost: 'http://10.100.207.13:8082/' // Pruebas
+    //dynamicHost: 'localhost:8080', // Desarrollo
+    dynamicHost: 'http://10.100.207.13:8082/' // Pruebas
   };
